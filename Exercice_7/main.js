8a4623c0301f4a595b65448bb1642242c0fc51c4
AnaisAffolter <affolteranais@gmail.com>
2023-09-27 11:30:47 +0200

    Adding exercise 7

---
+++ Exercice_7/main.js
@@ -0,0 +1,7 @@
+let overwatchCharacterProfil= {
+    firstName : "Angela",
+    lastName : "Ziegler",
+    age : 37,
+    place : "Suisse"
+}
+console.log(overwatchCharacterProfil.firstName + " " + overwatchCharacterProfil.lastName + " a " + overwatchCharacterProfil.age + " ans et elle vie en " + overwatchCharacterProfil.place + ".")
