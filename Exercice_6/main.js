22e61634e3542e60fae2900ce86e2dac87b9b43c
AnaisAffolter <affolteranais@gmail.com>
2023-09-27 11:00:39 +0200

    Adding exercise 6

---
+++ Exercice_6/main.js
@@ -0,0 +1,16 @@
+let names = []
+names.push("Vincent","Paul","Arthur")
+
+for(let i = 0; i < names.length; i++){
+    names[i] += " va à la pêche"
+    console.log (names[i])
+}
+// console.log(names)
+
+
+
+
+names.forEach(name => {
+    name += " et au tennis"
+    console.log (name)
+})
