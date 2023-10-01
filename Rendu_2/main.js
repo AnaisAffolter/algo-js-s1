1308e502aa6681ee1f19301b85e441453c90d737
AnaisAffolter <affolteranais@gmail.com>
2023-10-01 17:35:32 +0200

    Update exercise "rendu 2" for the weekend
    I modified the value of (death, damage, deathDamage)
    I updated the cod and now it's working.

--- Rendu_2/main.js
+++ Rendu_2/main.js
@@ -18,10 +18,12 @@ Les survivants ont gagné mais RIP à X, X, X…) */
 
 
 class Character {
-    
-    isAlive = true 
 
-    constructor(death, damage, deathDamage, name, caracteristic){
+    isAlive = true
+    damageValue = 10
+    deathDamageValue = 15
+
+    constructor(death, damage, deathDamage, name, caracteristic) {
         this.death = death
         this.damage = damage
         this.deathDamage = deathDamage
@@ -29,65 +31,126 @@ class Character {
         this.caracteristic = caracteristic
     }
 
+    CalculateAction() {
 
-
-
-}  
-
-class Killer {
-        constructor(name, hp) {
-            this.name = name
-            this.hp = hp
-        } 
+        let rand = Math.random()
+        if (rand <= this.death) {
+            this.isAlive = false
+            console.log(jason.name + " a tué " + this.name)
+        }
+        else if (rand > this.death && rand < this.death + this.damage) {
+            jason.JasonDamage(this.damageValue)
+            console.log(this.name + " a esquivé et a infligé " + this.damageValue + " PV à " + jason.name)
+        }
+        else if (rand >= this.death + this.damage) {
+            jason.JasonDamage(this.deathDamageValue)
+            this.isAlive = false
+            console.log(jason.name + " a tué " + this.name + " mais " + this.name + " a infligé " + this.deathDamageValue + " PV à " + jason.name)
+        }
     }
 
+
+}
+
+class Killer {
+    constructor(name, hp) {
+        this.name = name
+        this.hp = hp
+    }
+
+    JasonDamage(damage) {
+        this.hp -= damage
+    }
+
+
+}
+
+
+let jason = new Killer("Jason", 100)
+
+let characterName = ["Paul", "Jean", "Benjamin", "Nathan", "Lucas", "Christophe", "Anne", "Julie", "Momo", "Christelle", "Elise", "Catrine", "Christophe", "Julien", "Danielle", "Fatou"]
+let characterCaracteristic = ["nerd", "sportif", "blonde", "chanceux", "peureux", "maladroit", "joyeux", "timide", "extravertie"]
+
+let firstSurvivor = new Character(0.3, 0.5, 0.2, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
+let secondSurvivor = new Character(0.1, 0.4, 0.5, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
+let thirdSurvivor = new Character(0.7, 0.2, 0.1, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
+let fourthSurvivor = new Character(0.4, 0.3, 0.3, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
+let fifthSurvivor = new Character(0.2, 0.2, 0.6, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
+
+let survivor = [firstSurvivor, secondSurvivor, thirdSurvivor, fourthSurvivor, fifthSurvivor]
+
+let survivorStillAlive = true
+
+let currentSurvivor = 0
+
+let ripMessage = "les survivants ont gagné mais RIP a "
+
+
+/*console.log (jason)
+console.log (firstSurvivor)
+console.log (secondSurvivor)
+console.log (thirdSurvivor)
+console.log (fourthSurvivor)
+console.log (fifthSurvivor)
+console.log (survivor)*/
+
+/*firstSurvivor.isAlive = false
+console.log (survivor[0].isAlive)*/
+
+
+while (jason.hp > 0 && survivorStillAlive) {
+
+    console.log(jason.name + " attaque " + survivor[currentSurvivor].name)
+
+    survivor[currentSurvivor].CalculateAction()
+
+    for (let i = 0; i < survivor.length; i++) {
     
-    let jason = new Killer ("Jason", 100)
+        if (survivor[i].isAlive) {
 
-    let characterName = ["Paul", "Jean", "Benjamin", "Nathan", "Lucas", "Christophe", "Anne", "Julie", "Momo", "Christelle", "Elise", "Catrine", "Christophe", "Julien", "Danielle", "Fatou"]
-    let characterCaracteristic = ["nerd", "sportif", "blonde", "chanceux", "peureux", "maladroit", "joyeux", "timide", "extravertie"]
-    
-    let firstSurvivor = new Character (0.3, 0.5, 0.8, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
-    let secondSurvivor = new Character (0.2, 0.8, 0.6, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
-    let thirdSurvivor = new Character (0.7, 0.2, 0.4, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
-    let fourthSurvivor = new Character (0.4, 0.8, 0.7, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
-    let fifthSurvivor = new Character (0.8, 0.6, 0.2, characterName[Math.floor(Math.random() * 15)], characterCaracteristic[Math.floor(Math.random() * 8)])
-    
-    let survivor = [firstSurvivor, secondSurvivor, thirdSurvivor, fourthSurvivor, fifthSurvivor]
-    
-
-    console.log (jason)
-    console.log (firstSurvivor)
-    console.log (secondSurvivor)
-    console.log (thirdSurvivor)
-    console.log (fourthSurvivor)
-    console.log (fifthSurvivor)
-    console.log (survivor)
-
-
-    /*firstSurvivor.isAlive = false
-    console.log (survivor[0].isAlive)
-     console.log (jason.name + "atta")*/
-
-         while (jason.hp > 0 /*|| survivor.isAlive = false*/ ) {
-           
-
-
-
-                
+            survivorStillAlive = true
             break
 
-                        
-
-                        
-
+        } else {
+            survivorStillAlive = false
         }
+    }
+    if (!survivor[currentSurvivor].isAlive) {
+        currentSurvivor += 1
+    }
+}
+
+if (jason.hp <= 0) {
+    console.log(jason.name + " est mort !!!")
+
+    for (let i = 0; i < survivor.length; i++) {
+        if (!survivor[i].isAlive) {
+            ripMessage += survivor[i].name + ", "
+        }
+    }
+    console.log(ripMessage)
+}
+
+if (!survivorStillAlive) {
+    console.log(jason.name + " a tué tous les survivants... dommage, il lui restait " + jason.hp + " PV.")
+}
 
 
 
 
-   
-    
-    
+/*for (let i = 0; survivor.length(); i++) {
+
+    if (survivor[i].isAlive) {
+
+        survivorStillAlive = true 
+        break      
+
+    } else {
+        survivorStillAlive = false
+    }*/
+
+
+
+
 
 
